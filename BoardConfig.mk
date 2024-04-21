Amritorock/device_realme_r5x_recovery

r5x: Sort kernel cmdline and additional flags as a list
 * This allows better readability and eases tracking further changes.

Change-Id: I3a23f7ccb238b5f0e18dfa18080f1ae8ff51109f

BoardConfig.mk
@@ -80,7 +80,23 @@ PLATFORM_VERSION := 16.1.0
 PLATFORM_VERSION_LAST_STABLE := $(PLATFORM_VERSION)
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a600000.dwc3 firmware_class.path=/vendor/firmware_mnt/image earlycon=msm_geni_serial,0x880000 loop.max_part=7 cgroup.memory=nokmem,nosocket androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := \
+    console=ttyMSM0,115200n8 \
+    androidboot.hardware=qcom \
+    androidboot.console=ttyMSM0 \
+    androidboot.memcg=1 \
+    lpm_levels.sleep_disabled=1 \
+    video=vfb:640x400,bpp=32,memsize=3072000 \
+    msm_rtb.filter=0x237 \
+    service_locator.enable=1 \
+    swiotlb=1 \
+    androidboot.usbcontroller=a600000.dwc3 \
+    firmware_class.path=/vendor/firmware_mnt/image \
+    earlycon=msm_geni_serial,0x880000 \
+    loop.max_part=7 \
+    cgroup.memory=nokmem,nosocket \
+    androidboot.selinux=permissive
+
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100

eun0115/twrp_realme_r5x

BoardConfig.mk
@@ -42,7 +42,25 @@ TARGET_SCREEN_DENSITY := 320
 # Kernel
 BOARD_BOOTIMG_HEADER_VERSION := 1
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 console=null androidboot.hardware=qcom androidboot.console=ttyMSM0 lpm_levels.sleep_disabled=1 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 earlycon=msm_geni_serial,0x4a90000 loop.max_part=7 cgroup_disable=pressure cgroup.memory=nokmem,nosocket androidboot.android_dt_dir=/non-existent androidboot.boot_devices=soc/4744000.sdhci androidboot.init_fatal_reboot_target=recovery kpti=off androidboot.super_partition=system
+BOARD_KERNEL_CMDLINE := \
+     console=ttyMSM0,115200n8 \
+     console=null \
+     androidboot.hardware=qcom \
+     androidboot.console=ttyMSM0 \
+     lpm_levels.sleep_disabled=1 \
+     msm_rtb.filter=0x237 \
+     service_locator.enable=1 \
+     swiotlb=1 \
+     earlycon=msm_geni_serial,0x4a90000 \
+     loop.max_part=7 \
+     cgroup_disable=pressure \
+     cgroup.memory=nokmem,nosocket \
+     androidboot.android_dt_dir=/non-existent \
+     androidboot.boot_devices=soc/4744000.sdhci \
+     androidboot.init_fatal_reboot_target=recovery \
+     kpti=off \
+     androidboot.super_partition=system
+
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100